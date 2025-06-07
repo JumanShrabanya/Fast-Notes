6b0c4820846a7ddd16d0cd16c276ae876a8fe844
Juman Shrabanya <jumanshrabanya7@gmail.com>
2025-06-07 22:22:02 +0530

    tailwind css setup

---
+++ frontend/src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="text-cyan-600 text-6xl">Hello</h1>
+    </div>
+  );
+};
+
+export default Homepage;
